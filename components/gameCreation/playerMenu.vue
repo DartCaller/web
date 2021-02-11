3208e30549734051cc88be955caf8faa5235cd9d
8BitJonny <timon99@gmx.de>
2021-02-11 22:08:07 +0100

    fix(core): fix broken 'add Player' Button markup

--- components/gameCreation/playerMenu.vue
+++ components/gameCreation/playerMenu.vue
@@ -5,25 +5,18 @@
       <playerRow />
     </div>
     <div class="addPlayer">
-      <styled-button>
-        <div class="flex">
-          <plusSvg />
-          <p>Add Player</p>
-        </div>
-      </styled-button>
+      <styled-button icon="plus"> Add Player </styled-button>
     </div>
   </div>
 </template>
 
 <script>
-import plusSvg from '~/assets/images/plus.svg?inline'
 import styledButton from '~/components/common/StyledButton.vue'
 import playerRow from '~/components/gameCreation/modules/playerRow.vue'
 
 export default {
   name: 'PlayerMenu',
   components: {
-    plusSvg,
     styledButton,
     playerRow,
   },
@@ -42,14 +35,9 @@ h2 {
   width: 100%;
   text-align: left;
 
-  .flex {
-    display: flex;
-    align-items: center;
-    margin-left: -4px;
-
-    *:first-child {
-      margin-right: 23px;
-    }
+  /deep/ .vs-button__content svg {
+    margin-right: 20px;
+    margin-left: -1px;
   }
 }
 </style>
