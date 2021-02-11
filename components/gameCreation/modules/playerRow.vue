b7954114b8d707e966fa868b561f7a1f0378b320
8BitJonny <timon99@gmx.de>
2021-02-11 21:12:32 +0100

    feat(core): build game creation screen

---
+++ components/gameCreation/modules/playerRow.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row__container">
+    <div>
+      <vs-avatar size="30" color="#303030">
+        <img src="~/assets/images/player.png" alt="player image" />
+      </vs-avatar>
+      <p class="name">Lewe</p>
+    </div>
+    <div>
+      <p class="gamesPlayed">
+        {{ Math.floor(Math.random() * Math.floor(1000)) }} games
+      </p>
+      <delete-svg />
+    </div>
+  </div>
+</template>
+
+<script>
+import deleteSvg from '~/assets/images/x.svg?inline'
+
+export default {
+  name: 'PlayerRow',
+  components: { deleteSvg },
+}
+</script>
+
+<style scoped lang="scss">
+.row__container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > * {
+    display: flex;
+    align-items: center;
+
+    > * {
+      margin: 0 10px;
+    }
+  }
+}
+</style>
---
+++ components/gameCreation/playerMenu.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2>Players</h2>
+    <div class="player_list">
+      <playerRow />
+    </div>
+    <div class="addPlayer">
+      <styled-button>
+        <div class="flex">
+          <plusSvg />
+          <p>Add Player</p>
+        </div>
+      </styled-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import plusSvg from '~/assets/images/plus.svg?inline'
+import styledButton from '~/components/common/StyledButton.vue'
+import playerRow from '~/components/gameCreation/modules/playerRow.vue'
+
+export default {
+  name: 'PlayerMenu',
+  components: {
+    plusSvg,
+    styledButton,
+    playerRow,
+  },
+  data: () => ({
+    players: [],
+  }),
+}
+</script>
+
+<style scoped lang="scss">
+h2 {
+  margin-bottom: 15px;
+}
+
+.addPlayer {
+  width: 100%;
+  text-align: left;
+
+  .flex {
+    display: flex;
+    align-items: center;
+    margin-left: -4px;
+
+    *:first-child {
+      margin-right: 23px;
+    }
+  }
+}
+</style>
