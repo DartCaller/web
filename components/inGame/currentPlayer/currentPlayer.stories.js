888f3a5190f1b879ad2763d588cb85c153ee8748
8BitJonny <timon99@gmx.de>
2021-05-08 23:58:04 +0200

    feat(storybook): add currentPlayer.stories.js

---
+++ components/inGame/currentPlayer/currentPlayer.stories.js
@@ -0,0 +1,25 @@
+import CurrentPlayer from './currentPlayer.vue'
+
+export default {
+  component: CurrentPlayer,
+  title: 'CurrentPlayer',
+  exludeStories: /.*Data$/,
+  decorators: [
+    () => ({
+      template:
+        '<div style="width: fit-content; height: 19rem; margin: 1em; padding: 2rem; border-radius: 1rem; background-color: #303030"><story/></div>',
+    }),
+  ],
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { CurrentPlayer },
+  props: Object.keys(argTypes),
+  template: '<currentPlayer v-bind="$props"/>',
+})
+export const Default = Template.bind({})
+Default.args = {
+  scoresPerDart: [19, 29, 120],
+  currentPlayer: 'Bob',
+  pointsRemaining: 140,
+}
