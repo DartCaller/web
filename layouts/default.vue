9750d31c361ba22f6f48c2f19e40715ae8d96921
8BitJonny <timon99@gmx.de>
2021-05-08 20:56:24 +0200

    feat(css): extract default css into global.scss

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,40 +32,6 @@ export default {
 </script>
 
 <style lang="scss">
-html {
-  font-family: 'Tahoma', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-html,
-body,
-#__nuxt,
-#__layout {
-  height: 100%;
-  background-color: $grey;
-  background-image: url('~assets/images/DartboardBackground.svg');
-  background-position: center;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-  color: white;
-}
-
-a {
-  text-decoration: none;
-}
-
 .box_shadow {
   box-shadow: 0 0 30px 1px rgba(0, 0, 0, 0.6);
 }
@@ -90,31 +56,5 @@ a {
   position: absolute;
   width: 100%;
   height: 100%;
-}
-
-.vs-input,
-.vs-input::placeholder {
-  font-family: 'Tahoma', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  font-size: 18px;
-  font-weight: normal;
-}
-
-.vs-select__message {
-  position: absolute;
-  padding-top: 3px;
-}
-
-.vs-input__message {
-  position: absolute;
-  bottom: -15px;
-}
-
-.vs-input {
-  padding: 5px 13px;
-
-  &:focus {
-    padding-left: 10px;
-  }
 }
 </style>
