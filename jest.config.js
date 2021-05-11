0ee1d226fc0365dee100b967a12d9b61db689bb1
8BitJonny <timon99@gmx.de>
2021-05-11 12:35:47 +0200

    feat(dep): add cypress

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     '^.+/(.*\\.svg)\\?inline$': '<rootDir>/assets/images/$1',
     '^!style-loader!css-loader!sass-loader!(.*\\.scss)$': '$1',
   },
+  testPathIgnorePatterns: ['/test/e2e'],
   transformIgnorePatterns: ['/node_modules/(?!(@storybook/.*\\.vue$)|vuesax)'],
   moduleFileExtensions: ['vue', 'js', 'jsx', 'json', 'node'],
 }
