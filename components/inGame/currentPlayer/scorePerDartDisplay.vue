30095548b9ba1f6fcc466311c0c5fa9b81cb078b
8BitJonny <timon99@gmx.de>
2021-05-14 13:27:48 +0200

    fix(score-per-dart-display): fix no display of 0 score

--- components/inGame/currentPlayer/scorePerDartDisplay.vue
+++ components/inGame/currentPlayer/scorePerDartDisplay.vue
@@ -4,10 +4,10 @@
       v-for="i in 3"
       :key="i"
       class="dart"
-      :class="{ active: scoresPerDart[i - 1] }"
+      :class="{ active: stringScores[i - 1] }"
     >
       <dart-svg class="dartSvg" />
-      <p class="score">{{ scoresPerDart[i - 1] || '_' }}</p>
+      <p class="score">{{ stringScores[i - 1] || '_' }}</p>
     </div>
   </div>
 </template>
@@ -24,6 +24,11 @@ export default {
     scoresPerDart: {
       type: Array,
       required: true,
+    },
+  },
+  computed: {
+    stringScores() {
+      return this.scoresPerDart.map((i) => i !== undefined && i.toString())
     },
   },
 }
