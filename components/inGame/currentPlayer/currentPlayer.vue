f0030db0722fe1e5c0c44ef2df0d055f4926fe26
8BitJonny <timon99@gmx.de>
2021-05-14 20:49:06 +0200

    feat(current-player): add RemainingScore animation

--- components/inGame/currentPlayer/currentPlayer.vue
+++ components/inGame/currentPlayer/currentPlayer.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="current__player__container">
     <h1 class="colored">{{ currentPlayer }}</h1>
-    <h2>{{ pointsRemaining }} Points remaining</h2>
+    <h2><span id="current__player__score">0</span> Points remaining</h2>
     <scorePerDartDisplay :scores-per-dart="scoresPerDart" />
   </div>
 </template>
 
 <script>
+import { CountUp } from 'countup.js'
 import scorePerDartDisplay from '~/components/inGame/currentPlayer/scorePerDartDisplay.vue'
 
 export default {
@@ -27,6 +28,22 @@ export default {
       type: Number,
       required: true,
     },
+  },
+  data: () => ({
+    countAnimation: null,
+  }),
+  watch: {
+    pointsRemaining(newV, oldV) {
+      this.countAnimation.update(newV)
+    },
+  },
+  mounted() {
+    this.countAnimation = new CountUp(
+      'current__player__score',
+      this.pointsRemaining,
+      { startVal: this.pointsRemaining, duration: 1 }
+    )
+    this.countAnimation.start()
   },
 }
 </script>
