598856288e2bd36597fb49db1bdb1197d2ab5916
8BitJonny <timon99@gmx.de>
2021-05-27 23:04:02 +0200

    fix(eslint): fix linting

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,10 +27,11 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [].concat(process.env.NODE_ENV === 'production' ? [] : [
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/stylelint-module',
-  ]),
+  buildModules: [].concat(
+    process.env.NODE_ENV === 'production'
+      ? []
+      : ['@nuxtjs/eslint-module', '@nuxtjs/stylelint-module']
+  ),
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
