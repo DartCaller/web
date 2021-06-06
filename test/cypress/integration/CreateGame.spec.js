1bbd352f32ba990bcf57d0351a487cf6b26007d2
8BitJonny <timon99@gmx.de>
2021-06-06 13:56:15 +0200

    feat(create-game): add integration tests

---
+++ test/cypress/integration/CreateGame.spec.js
@@ -0,0 +1,36 @@
+const mockWebSocket = { send: (data) => ({}) }
+describe('CreateGame', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:6006/iframe.html?id=creategame--default')
+    cy.spy(mockWebSocket, 'send')
+    cy.window().then((win) => {
+      win.mockWebSocketSend = mockWebSocket.send
+    })
+  })
+
+  it('should be able to create a 501 game', () => {
+    cy.addRowAndTypePlayerName('Alice', { addRow: false })
+    cy.addRowAndTypePlayerName('Bob')
+    cy.getByData('create-game__submit').click()
+    cy.wrap({}).should(() => {
+      expect(mockWebSocket.send).to.be.calledWithExactly(
+        '{"type":"CreateGame","players":["Alice","Bob"],"gameMode":501}'
+      )
+    })
+  })
+
+  it('should be able to create a 301 game', () => {
+    cy.addRowAndTypePlayerName('Alice', { addRow: false })
+    cy.addRowAndTypePlayerName('Bob')
+    cy.addRowAndTypePlayerName('Clemens')
+    cy.getByData('create-game__game-mode').vsSelect(
+      'create-game__game-mode__option_301'
+    )
+    cy.getByData('create-game__submit').click()
+    cy.wrap({}).should(() => {
+      expect(mockWebSocket.send).to.be.calledWithExactly(
+        '{"type":"CreateGame","players":["Alice","Bob","Clemens"],"gameMode":301}'
+      )
+    })
+  })
+})
--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -12,3 +12,23 @@ Cypress.Commands.add('getByData', (dataSelector, options = {}) => {
   const extraSelector = options.extraSelector ? options.extraSelector : ''
   cy.get(`[data-test=${dataSelector}]${extraSelector}`, options)
 })
+
+Cypress.Commands.add(
+  'addRowAndTypePlayerName',
+  (name, { addRow = true } = {}) => {
+    if (addRow) cy.getByData('player-menu__add-row').click()
+    cy.getByData('player-row__input')
+      .last()
+      .type(name + '{enter}')
+  }
+)
+
+// Special Util function to select values from Vuesax dropdown menus
+Cypress.Commands.add(
+  'vsSelect',
+  { prevSubject: 'element' },
+  (subject, optionSelector) => {
+    cy.wrap(subject).click()
+    cy.getByData(optionSelector, { extraSelector: ':visible' }).click()
+  }
+)
