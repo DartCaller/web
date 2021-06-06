e3308e380fe3fb97f8c4a6b45694dcec0c928255
8BitJonny <timon99@gmx.de>
2021-06-06 13:18:03 +0200

    feat(create-game): extracted into component

--- pages/create-game.vue
+++ pages/create-game.vue
@@ -1,35 +1,14 @@
 <template>
-  <div class="create_game__container">
-    <h1>Create Game</h1>
-    <div class="cards__container">
-      <div class="player__card box_shadow">
-        <playerMenu @input="players = $event" />
-      </div>
-      <div class="settings__card box_shadow">
-        <settingsMenu @input="settings = $event" />
-      </div>
-    </div>
-    <styled-button primary :loading="loading" @click.native="startGame"
-      >Start Game</styled-button
-    >
-  </div>
+  <create-game />
 </template>
 
 <script>
-import styledButton from '~/components/common/StyledButton.vue'
-import playerMenu from '~/components/gameCreation/playerMenu.vue'
-import settingsMenu from '~/components/gameCreation/settingsMenu.vue'
+import CreateGame from '~/components/gameCreation/createGame'
 
 export default {
-  name: 'CreateGame',
-  components: { styledButton, playerMenu, settingsMenu },
+  name: 'CreateGamePage',
+  components: { CreateGame },
   middleware: ['auth'],
-  data: () => ({
-    socket: null,
-    players: [],
-    settings: {},
-    loading: false,
-  }),
   async mounted() {
     try {
       await this.$socket.connect()
@@ -44,66 +23,5 @@ export default {
       })
     }
   },
-  methods: {
-    startGame() {
-      this.loading = true
-      this.$store.commit('game/SET_GAME_STATE', 'CREATE')
-      this.$socket.send({
-        type: 'CreateGame',
-        players: this.players.map((e) => e.name),
-        gameMode: this.settings.gameMode,
-      })
-      this.$socket.onMessage((data) => {
-        if (this.$store.state.game.gameState === 'CREATE') {
-          this.$store.commit('game/SET_SUBSCRIBED', true)
-          this.$router.push({ path: `/${data.gameID}/play` })
-          this.$store.commit('game/SET_GAME_STATE', 'PLAY')
-        }
-        this.$store.commit('game/SET_SERVER_STATE', data)
-      })
-    },
-  },
 }
 </script>
-
-<style scoped lang="scss">
-.create_game__container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-
-  h1 {
-    margin-top: 50px;
-  }
-
-  > * {
-    margin: 15px 0;
-  }
-}
-
-.cards__container {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  column-gap: 20px;
-  row-gap: 20px;
-  grid-template-areas: 'players settings';
-
-  .player__card,
-  .settings__card {
-    background-color: $grey;
-    border-radius: 10px;
-    padding: 20px 40px;
-    min-width: 35vw;
-    min-height: 40vh;
-  }
-
-  .player__card {
-    grid-area: players;
-  }
-
-  .settings__card {
-    grid-area: settings;
-  }
-}
-</style>
